7ab46d1367bac9fcdfa0bb1be84665f0c526ad75
Pavan Kumar C S <psiddegowda@clarku.edu>
2025-03-16 16:37:35 -0400

    init node, mongo and reactjs

---
+++ app/client/src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Login from './components/Login';
+import Register from './components/Register';
+
+const App = () => {
+  return (
+    <div>
+      <Register />
+      <Login />
+    </div>
+  );
+};
+export default App;
---
+++ app/index.js
@@ -0,0 +1,17 @@
+// index.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./db');
+const authController = require('./controllers/authController');
+
+const app = express();
+const port = 5000;
+app.use(cors());
+app.use(bodyParser.json());
+connectDB();
+app.post('/register', authController.registerUser);
+app.post('/login', authController.loginUser);
+app.listen(port, ()=> {
+  console.log(`Server running on port ${port}`);
+});
